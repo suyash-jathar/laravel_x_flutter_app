50af07a9baba6b7c252622050c69b384e3c71d44
suyash-jathar <suyashjathar15@gmail.com>
2023-04-09 10:48:30 +0530

    Added Posts,Comment,Like routes

--- blog-laravel-app/routes/api.php
+++ blog-laravel-app/routes/api.php
@@ -25,5 +25,22 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/user',[AuthController::class,'user']);
     Route::post('/logout',[AuthController::class,'logout']);
 
+    // Posts
+    Route::get('/posts',[PostController::class,'index']); // get all posts
+    Route::post('/posts',[PostController::class,'store']); // create a post
+    Route::get('/posts/{id}',[PostController::class,'show']); // get a single post
+    Route::put('/posts/{id}',[PostController::class,'update']); // update a post
+    Route::delete('/posts/{id}',[PostController::class,'destroy']); // delete a post
+
+    // Comments
+    Route::get('/posts/{id}/comments',[CommentController::class,'index']); // get all comments
+    Route::post('/posts/{id}/comments',[CommentController::class,'store']); // create a comment
+    Route::put('/comments/{id}',[CommentController::class,'update']); // update a comment
+    Route::delete('/comments/{id}',[CommentController::class,'destroy']); // delete a comment
+
+    // Likes
+    Route::get('/posts/{id}/likes',[LikeController::class,'likeOrUnlike']); // get all likes
+
+
 });
 
